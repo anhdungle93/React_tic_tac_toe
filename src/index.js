bcceb3b3eb6deda78cb52273bfabd1c88e6c28bd
anhdungle <anhdungle634@gmail.com>
2020-06-04 09:31:41 +0200

    move handleClick to Game component

--- src/index.js
+++ src/index.js
@@ -13,17 +13,7 @@ function Square(props) {
 
 class Board extends React.Component {
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
+
   renderSquare(i) {
     return <Square value={this.props.squares[i]} 
     onClick={() => this.props.onClick(i)}
@@ -34,7 +24,6 @@ class Board extends React.Component {
     
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -64,6 +53,20 @@ class Game extends React.Component {
       }],
       xIsNext: true,
     };
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{squares: squares,}]),
+      xIsNext: !this.state.xIsNext,
+    });
   }
   render() {
     const history = this.state.history;
